4a152c6138f4927404fffa644abdf2903a2eeca9
Dami Lee <50016477+iamdami@users.noreply.github.com>
2022-01-04 02:54:39 +0900

    Create change.py

---
+++ Greedy/change.py
@@ -0,0 +1,14 @@
+# 거슬러줘야 할 동전의 최소 개수 구하기
+
+n = 1260
+count = 0
+coin = 0
+
+# 큰 단위 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n // coin  # 해당 화폐로 거슬러 줄 수 있는 동전 개수 세기
+    n %= coin
+
+print(count)  # 500*2, 100*2, 50*1, 10*1 => 6
