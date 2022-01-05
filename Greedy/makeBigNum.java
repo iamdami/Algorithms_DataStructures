3f447a7586fc5766b3094d3454efd04473703c7a
Dami Lee <50016477+iamdami@users.noreply.github.com>
2022-01-05 20:01:42 +0900

    Create makeBigNum.java

---
+++ Greedy/makeBigNum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); 
+        int first = arr[n - 1]; // 가장 큰 수
+        int second = arr[n - 2]; // 두 번째로 큰 수
+
+        // 가장 큰 수 더해지는 횟수 계산
+        int cnt = (m / (k + 1)) * k;
+        cnt += m % (k + 1);
+
+        int result = 0;
+        result += cnt * first; // 가장 큰 수 더하기
+        result += (m - cnt) * second; // 두 번째로 큰 수 더하기
+
+        System.out.println(result);
+    }
+
+}
