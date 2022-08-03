4ad9087c37e3914e51f0b1d0bfe102f287cae85c
Dami Lee <onemoredami@gmail.com>
2022-08-03 22:06:43 +0900

    Update binarySearch.py

--- Search/binarySearch.py
+++ Search/binarySearch.py
@@ -7,7 +7,7 @@ def binarySearch(arr, target, start, end):
   elif arr[mid] > target:
     return binarySearch(arr, target, start, mid - 1)  # 중간점 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
   else:
-    return binary_search(arr, target, mid + 1, end)  # 중간점 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인  
+    return binarySearch(arr, target, mid + 1, end)  # 중간점 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인  
   
 n, target = list(map(int, input().split()))  # 원소 개수와 찾고자 하는 값 입력 받기
 arr = list(map(int, input().split()))  # 전체 원소 입력 받기
