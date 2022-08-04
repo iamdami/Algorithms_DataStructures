5aa60a5b7de53a4b0f781795b739630c73e96f63
Dami Lee <onemoredami@gmail.com>
2022-08-04 21:55:07 +0900

    Add files via upload

---
+++ DynamicProgramming/fiboUsingMemoization.py
@@ -0,0 +1,14 @@
+# 메모이제이션 이용하면 피보나치 수열 함수의 시간 복잡도는 O(N)
+
+d = [0] * 100
+
+def fibo(x):
+    print(f"f({str(x)})", end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+fibo(6)
